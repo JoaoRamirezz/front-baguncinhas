d81391d91381493ba499823621a2509dab96a50f
m4ite <maitesenaibosch@gmail.com>
2023-10-31 16:13:52 -0300

    Fixing SignUp page

--- pages/SignUp/SIgnUp.js
+++ pages/SignUp/SIgnUp.js
@@ -3,63 +3,78 @@ import {
   Text,
   View,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
 } from "react-native";
 import { useState } from "react";
 import axios from "axios";
 
 export function SignUp(props) {
-  const [nome, setNome] = useState("");
+  const [firstName, setNome] = useState("");
+  const [lastName, setSobrenome] = useState("");
   const [age, setAge] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [cpf, setCPF] = useState("");
 
   const signUp = async () => {
     // Password didn't match
-    if (password !== confirmPassword)
-      return;
+    if (password !== confirmPassword) return;
 
     // Invalid data
-    if (!nome || !age || !email || !password)
-      return;
-    
+    if (!firstName || !age || !email || !password || !cpf || !lastName) return;
+
     const data = {
       email,
-      firstName: 'string',
-      lastName: 'string',
-      password: 'string',
-      CPF: 'string2',
-    }
+      firstName,
+      lastName,
+      password,
+      birthday: new Date(age),
+      CPF: cpf,
+    };
 
-    const res = await axios.post('http://localhost:8080/api/user/', data);
+    const res = await axios.post("http://localhost:8080/api/user/", data);
     if (res.status === 201) {
-      props.navigation.navigate("Login")
+      props.navigation.navigate("Login");
     }
-  }
+  };
 
   return (
     <View style={styleSignUp.bg}>
+      <View style={styleSignUp.inputs1}>
+        <View>
+          <Text style={styleSignUp.textView}>Primeiro nome: </Text>
+          <TextInput
+            style={styleSignUp.input}
+            maxLength={20}
+            onChangeText={(text) => setNome(text)}
+          />
+        </View>
 
-      <Text style={styleSignUp.textView}>Nome</Text>
-      <TextInput
-        style={styleSignUp.input}
-        multiline
-        maxLength={20}
-        numberOfLines={2}
-        width={10}
-        onChangeText={(text) => setNome(text)}
-      />
+        <View>
+          <Text style={styleSignUp.textView}>Ultimo nome: </Text>
+          <TextInput
+            style={styleSignUp.input}
+            maxLength={20}
+            onChangeText={(text) => setSobrenome(text)}
+          />
+        </View>
+      </View>
 
       <View style={styleSignUp.inputs1}>
         <View>
-          <Text style={styleSignUp.textView}>Idade:</Text>
+          <Text style={styleSignUp.textView}>Data nascimento:</Text>
           <TextInput
-            style={styleSignUp.input2}
-            multiline
-            numberOfLines={1}
-            width={20}
+            style={styleSignUp.input}
             onChangeText={(text) => setAge(text)}
+          />
+        </View>
+
+        <View>
+          <Text style={styleSignUp.textView}>CPF:</Text>
+          <TextInput
+            style={styleSignUp.input}
+            onChangeText={(text) => setCPF(text)}
           />
         </View>
       </View>
@@ -68,10 +83,7 @@ export function SignUp(props) {
         <Text style={styleSignUp.textView}>Email:</Text>
         <TextInput
           style={styleSignUp.input}
-          multiline
           maxLength={20}
-          numberOfLines={2}
-          width={10}
           onChangeText={(text) => setEmail(text)}
         />
       </View>
@@ -80,10 +92,7 @@ export function SignUp(props) {
         <Text style={styleSignUp.textView}>Senha:</Text>
         <TextInput
           style={styleSignUp.input}
-          multiline
           maxLength={20}
-          numberOfLines={2}
-          width={10}
           onChangeText={(text) => setPassword(text)}
         />
       </View>
@@ -92,10 +101,7 @@ export function SignUp(props) {
         <Text style={styleSignUp.textView}>Confirmar Senha:</Text>
         <TextInput
           style={styleSignUp.input}
-          multiline
           maxLength={20}
-          numberOfLines={2}
-          width={10}
           onChangeText={(text) => setConfirmPassword(text)}
         />
       </View>
@@ -116,62 +122,21 @@ export function SignUp(props) {
   );
 }
 
-
 const styleSignUp = StyleSheet.create({
-  
-  Logo:{
-    width: 400,
-    height: 250
+  bg: {
+    backgroundColor: "#36CECC",
+    height: "100%",
+    display: "flex"
   },
 
 
   input: {
-    height: 40,
-    marginLeft: 40,
-    marginRight: 40,
+    marginHorizontal: 40,
     marginTop: 10,
     padding: 10,
-    borderRadius: 5,
     backgroundColor: "#65F4F2",
-    height: 50,
+    height: 45,
     borderRadius: 20
-  },
-
-  input2: {
-    marginLeft: 40,
-    marginTop: 10,
-    padding: 10,
-    borderRadius: 5,
-    backgroundColor: "#65F4F2",
-    height: 50,
-    borderRadius: 20
-  },
-
-  input3: {
-    marginLeft: 10,
-    marginTop: 10,
-    padding: 10,
-    borderRadius: 5,
-    backgroundColor: "#65F4F2",
-    height: 50,
-    borderRadius: 20
-  },
-
-  bg: {
-    backgroundColor: "#36CECC",
-    height: "100%",
-    display: "flex",
-    justifyContent: "space-between",
-  },
-
-  bgUsers: {
-    backgroundColor: "lightgray",
-    height: "100%",
-  },
-
-  inputs1: {
-    flexDirection: "row",
-    justifyContent: "space-arround",
   },
 
   textView: {
@@ -179,33 +144,20 @@ const styleSignUp = StyleSheet.create({
     marginTop: 20,
   },
 
-  switch: {
-    marginLeft: 12,
-  },
-
   button: {
     padding: 10,
-    margin: 12,
+    marginHorizontal: 40,
+    marginTop: 40,
     backgroundColor: "#176585",
     borderRadius: 20
   },
+
   button1: {
     padding: 10,
-    margin: 12,
+    margin: 12
   },
 
   textbutton: {
-    textAlign: "center",
-  },
-
-  title: {
-    marginTop: "10%",
-    marginBottom: "10%",
-    fontSize: 100,
-    textAlign: "center",
-  },
-
-  titleUsers: {
-    fontSize: 70,
-  },
-})
+    textAlign: "center"
+  }
+});
